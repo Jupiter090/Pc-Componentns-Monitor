8d56e628e9d8492bb1ec8077fef302e534713f15
Jupiter090 <matkopla0501@gmail.com>
2022-05-08 14:23:11 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace PcComponentnsStats
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.cpu_name = new System.Windows.Forms.Label();
+            this.cpu_temp = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cpu_name
+            // 
+            this.cpu_name.AutoSize = true;
+            this.cpu_name.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.cpu_name.Location = new System.Drawing.Point(48, 58);
+            this.cpu_name.Name = "cpu_name";
+            this.cpu_name.Size = new System.Drawing.Size(164, 32);
+            this.cpu_name.TabIndex = 0;
+            this.cpu_name.Text = "{cpu_name}";
+            // 
+            // cpu_temp
+            // 
+            this.cpu_temp.AutoSize = true;
+            this.cpu_temp.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.cpu_temp.Location = new System.Drawing.Point(48, 100);
+            this.cpu_temp.Name = "cpu_temp";
+            this.cpu_temp.Size = new System.Drawing.Size(156, 32);
+            this.cpu_temp.TabIndex = 1;
+            this.cpu_temp.Text = "{cpu_temp}";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(51, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 32);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "CPU:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cpu_temp);
+            this.Controls.Add(this.cpu_name);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label cpu_name;
+        private System.Windows.Forms.Label cpu_temp;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PcComponentnsStats
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            double temperature = 0;
+            //Create new ManagementObjectSearcher
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(@"root\WMI", "SELECT * FROM MSAcpi_ThermalZoneTemperature");
+            foreach (ManagementObject obj in searcher.Get())
+            {
+                //Gets the cpu temp and converts it to °C
+                temperature = Convert.ToDouble(obj["CurrentTemperature"].ToString());
+                temperature = (temperature - 2732) / 10.0;
+            }
+            //Will set the text to the temperatures
+            cpu_temp.Text = "Temp: " + temperature.ToString() + "°C";
+
+            //Thing to get the processor name (Idk what it does I just copied it.)
+            ManagementClass mc = new ManagementClass("win32_processor");
+            ManagementObjectCollection moc = mc.GetInstances();
+
+            //Resets the timer
+            Timer timer  = sender as Timer;
+            timer.Interval = 5000;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            double temperature = 0;
+            //Create new ManagementObjectSearcher
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(@"root\WMI", "SELECT * FROM MSAcpi_ThermalZoneTemperature");
+            foreach (ManagementObject obj in searcher.Get())
+            {
+                //Gets the cpu temp and converts it to °C
+                temperature = Convert.ToDouble(obj["CurrentTemperature"].ToString());
+                temperature = (temperature - 2732) / 10.0;
+            }
+            //Will set the text to the temperatures
+            cpu_temp.Text = "Temp: " + temperature.ToString() + "°C";
+        }
+    }
+}
