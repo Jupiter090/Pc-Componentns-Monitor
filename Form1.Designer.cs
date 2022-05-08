2ddc5cbd2a721c2af022831fdfafbfd1b7a9ff13
Jupiter090 <matkopla0501@gmail.com>
2022-05-08 14:59:28 +0200

    Update

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,25 +33,30 @@
             this.cpu_temp = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // cpu_name
             // 
             this.cpu_name.AutoSize = true;
-            this.cpu_name.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.cpu_name.Location = new System.Drawing.Point(48, 58);
+            this.cpu_name.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.cpu_name.Location = new System.Drawing.Point(12, 50);
             this.cpu_name.Name = "cpu_name";
-            this.cpu_name.Size = new System.Drawing.Size(164, 32);
+            this.cpu_name.Size = new System.Drawing.Size(80, 16);
             this.cpu_name.TabIndex = 0;
             this.cpu_name.Text = "{cpu_name}";
             // 
             // cpu_temp
             // 
             this.cpu_temp.AutoSize = true;
-            this.cpu_temp.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.cpu_temp.Location = new System.Drawing.Point(48, 100);
+            this.cpu_temp.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.cpu_temp.Location = new System.Drawing.Point(12, 66);
             this.cpu_temp.Name = "cpu_temp";
-            this.cpu_temp.Size = new System.Drawing.Size(156, 32);
+            this.cpu_temp.Size = new System.Drawing.Size(76, 16);
             this.cpu_temp.TabIndex = 1;
             this.cpu_temp.Text = "{cpu_temp}";
             // 
@@ -64,24 +69,71 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label1.Location = new System.Drawing.Point(51, 20);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(12, 31);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(85, 32);
+            this.label1.Size = new System.Drawing.Size(42, 16);
             this.label1.TabIndex = 2;
             this.label1.Text = "CPU:";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(490, -2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(36, 35);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(3, 2);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(242, 31);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "PC Components Stats";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.ForeColor = System.Drawing.Color.Black;
+            this.panel1.Location = new System.Drawing.Point(-3, -5);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(529, 33);
+            this.panel1.TabIndex = 5;
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(523, 121);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.cpu_temp);
             this.Controls.Add(this.cpu_name);
+            this.Controls.Add(this.panel1);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -93,6 +145,10 @@
         private System.Windows.Forms.Label cpu_temp;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -32,10 +32,6 @@ namespace PcComponentnsStats
             //Will set the text to the temperatures
             cpu_temp.Text = "Temp: " + temperature.ToString() + "°C";
 
-            //Thing to get the processor name (Idk what it does I just copied it.)
-            ManagementClass mc = new ManagementClass("win32_processor");
-            ManagementObjectCollection moc = mc.GetInstances();
-
             //Resets the timer
             Timer timer  = sender as Timer;
             timer.Interval = 5000;
@@ -43,6 +39,12 @@ namespace PcComponentnsStats
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            this.FormBorderStyle = FormBorderStyle.None;
+            Rectangle workingArea = Screen.GetWorkingArea(this);
+            this.Location = new Point(workingArea.Right - Size.Width,
+                                      workingArea.Bottom - Size.Height);
+            this.TopMost = true;
+
             double temperature = 0;
             //Create new ManagementObjectSearcher
             ManagementObjectSearcher searcher = new ManagementObjectSearcher(@"root\WMI", "SELECT * FROM MSAcpi_ThermalZoneTemperature");
@@ -54,6 +56,28 @@ namespace PcComponentnsStats
             }
             //Will set the text to the temperatures
             cpu_temp.Text = "Temp: " + temperature.ToString() + "°C";
+
+            //Thing to get the processor name (Idk what it does I just copied it.)
+            string cpuName = String.Empty;
+            ManagementClass mc = new ManagementClass("win32_processor");
+            ManagementObjectCollection managCollec = mc.GetInstances();
+            foreach (ManagementObject managObj in managCollec)
+            {
+                cpuName = managObj.Properties["Name"].Value.ToString();
+                cpu_name.Text = "Name: " + cpuName;
+                break;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            this.TopMost = true;
+            timer2.Interval = 1000;
         }
     }
 }
