86595e47b0857df3d2dee1633bacabcb7f168bf8
Jupiter090 <matkopla0501@gmail.com>
2022-05-10 15:33:03 +0200

    Test

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -64,5 +64,17 @@ namespace PcComponentnsStats.Properties {
                 return ((string)(this["Dfhgf4578hfg"]));
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string AhojXDS {
+            get {
+                return ((string)(this["AhojXDS"]));
+            }
+            set {
+                this["AhojXDS"] = value;
+            }
+        }
     }
 }
